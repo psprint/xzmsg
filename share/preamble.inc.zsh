1241434ee63779307b95c8f652cc9508c1b612ba
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2023-01-13 18:10:42 +0059

    overall uplift + themes

---
+++ share/preamble.inc.zsh
@@ -0,0 +1,30 @@
+#!/usr/bin/env zsh
+# -*- mode: sh;sh-indentation: 4;indent-tabs-mode: nil;sh-basic-offset: 4; -*-
+#
+# Copyright (c) 2023 Sebastian Gniazdowski
+#
+
+builtin emulate -L zsh
+builtin setopt extendedglob warncreateglobal typesetsilent \
+                noshortloops rcquotes noautopushd
+
+# Possibly fix $0 with a new trick – use of a %x prompt expansion
+0=${${(%):-%x}%/*~*/xzmsg.plugin.zsh}
+0=${${(M)0:#/*}:-$PWD/$0}
+
+: ${XZDIR:=$0:h} ${XZBIN_DIR::=${0:h}/bin} \
+       ${XZFUNC_DIR=::${0:h}/functions} \
+       ${XZAES_DIR=::${0:h}/aliases}
+
+# Unset helper function on exit
+builtin trap 'unset -f xzmsg_subst' EXIT
+
+# Mute possible create global warning
+local -a match mbegin mend reply
+local MATCH REPLY; integer MBEGIN MEND
+
+# Set up aliases (global, suffix and the proper ones)
+for REPLY in $XZAES_DIR/*[a-zA-Z];do
+    REPLY="$REPLY:t=$(<$REPLY)"
+    alias "${${REPLY#*=}%%:*}" "${(M)REPLY##[^=]##}=${REPLY#*:}"
+done
--- xzmsg.plugin.zsh
+++ xzmsg.plugin.zsh
@@ -1,7 +1,9 @@
+#!/usr/bin/env zsh
 # -*- mode: sh;sh-indentation: 4;indent-tabs-mode: nil;sh-basic-offset: 4; -*-
 #
 # Copyright (c) 2023 Sebastian Gniazdowski
 #
+
 # Possibly fix $0 with a new trick – use of a %x prompt expansion
 0=${(%):-%x}
 0=${${(M)0:#/*}:-$PWD/$0}
@@ -14,8 +16,9 @@ fi
 
 # Standard hash for plugins, to not pollute the namespace
 typeset -gA Plugins
-Plugins[XZMSG_DIR]="${0:h}"
-export XZMSG_DIR="${0:h}" \
+Plugins[XZDIR]="${0:h}"
+export XZDIR="${0:h}" \
+       XZBIN_DIR="${0:h}"/bin \
        XZFUNC_DIR="${0:h}"/functions \
        XZAES_DIR="${0:h}"/aliases
 
@@ -23,24 +26,23 @@ export XZMSG_DIR="${0:h}" \
 : ${APPNICK:=XZ} # Set for standard message preamble tag: [app][file:line]
 : ${XZCONF:=${XDG_CONFIG_HOME:-$HOME/.config}/xzmsg}
 : ${XZCACHE:=${XDG_CACHE_HOME:-$HOME/.cache}/xzmsg}
-: ${XZLOG:=$XZCACHE/${(L)APPZNICK}.log}
-: ${XZTHEME:=$XZMSG_DIR/themes/default.cfg}
+: ${XZINI:=$XZCACHE/${(L)APPZNICK}.xzc}
+: ${XZLOG:=$XZCACHE/${(L)APPZNICK}.xzl}
+: ${XZTHEME:=$XZDIR/themes/default.xzt}
+
 command mkdir -p $XZCONF $XZCACHE
 export APPNICK XZCONF XZCACHE XZLOG XZTHEME
 
 # Safe variables
 typeset -g REPLY reply=()
 
-# Set up aliases (global and the proper ones)
+# Set up aliases (global, suffix and the proper ones)
 for REPLY in $XZAES_DIR/*[a-zA-Z];do
     REPLY="$REPLY:t=$(<$REPLY)"
     alias "${${REPLY#*=}%%:*}" "${(M)REPLY##[^=]##}=${REPLY#*:}"
 done
 
-() {
-    builtin emulate -L zsh -o extendedglob
-    # Load any case (#i) and both files and symlinks (@ and .)
-    autoload -z $XZFUNC_DIR/(#i)*[a-z](N@:t) $XZFUNC_DIR/(#i)*[a-z](N.:t)
-}
+# Load any case (#i) and both files and symlinks (@ and .)
+autoload -z "$XZFUNC_DIR"/*[0-9A-Za-z](N:t.,@)
 
 # vim:ft=zsh:tw=80:sw=4:sts=4:et:foldmarker=[[[,]]]
